fe20f9a0dbc601c22e0ed86743ade2e8be48c6b0
Mehmet Burak Erman <mburakerman@gmail.com>
2023-12-12 01:26:52 +0300

    *

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,48 @@
-import styled from 'styled-components';
-import { SparklyText } from './SparklyText';
-import { FaGithub } from 'react-icons/fa';
+import styled from "styled-components";
+import { SparklyText } from "./SparklyText";
+import { FaGithub } from "react-icons/fa";
 
 const StyledSparkleSVG = styled.svg`
- display: flex;
- align-items: center;
- justify-content: center;
- margin: 0 auto;
- margin-top: 5rem;
- fill: #8BE9FD;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  margin-top: 5rem;
+  fill: #8be9fd;
 `;
 
 const StyledTitle = styled.h1`
   text-align: center;
   font-size: 30px;
   margin-bottom: 4rem;
-  margin-top:0;
+  margin-top: 0;
   font-weight: 500;
 `;
 
 const StyledGithubButton = styled.a`
   margin: 0 auto;
   width: max-content;
-  display: flex; 
+  display: flex;
   align-items: center;
   justify-content: center;
-  background-color: #FFFFFF;
+  background-color: #ffffff;
   border: 1px solid rgba(0, 0, 0, 0.1);
   box-shadow: rgba(0, 0, 0, 0.02) 0 1px 3px 0;
   color: rgba(0, 0, 0, 0.85);
   cursor: pointer;
   font-weight: 600;
-  padding:8px 18px;
+  padding: 8px 18px;
   text-decoration: none;
   border-radius: 8px;
   font-size: 18px;
-  transition: all .5s ease;
+  transition: all 0.5s ease;
 
-  &: hover {
-    background-color: rgba(255,255,255 ,.8);
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.8);
   }
 
   svg {
-    margin-right: .5em;
+    margin-right: 0.5em;
   }
 `;
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createGlobalStyle } from 'styled-components';
-import App from './App.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
+import App from "./App.tsx";
 
 const GlobalStyle = createGlobalStyle`
   :root {
@@ -19,7 +19,7 @@ const GlobalStyle = createGlobalStyle`
    }
 `;
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GlobalStyle />
     <App />
