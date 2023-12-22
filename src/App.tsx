60ef38a11f2a94ea36260ee8c8006c48bf11d265
Mehmet Burak Erman <mburakerman@gmail.com>
2023-12-22 15:22:03 +0300

    *

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,16 @@
-import styled from "styled-components";
 import { SparklyText } from "./SparklyText";
-
-const StyledTitle = styled.h1`
-  text-align: center;
-  font-size: 30px;
-  margin-bottom: 4rem;
-  margin-top: 100px;
-  font-weight: 500;
-`;
-
-const StyledGithubButton = styled.a`
-  margin: 0 auto;
-  width: max-content;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #ffffff;
-  border: 1px solid rgba(0, 0, 0, 0.1);
-  box-shadow: rgba(0, 0, 0, 0.02) 0 1px 3px 0;
-  color: rgba(0, 0, 0, 0.85);
-  cursor: pointer;
-  font-weight: 600;
-  padding: 8px 18px;
-  text-decoration: none;
-  border-radius: 8px;
-  font-size: 18px;
-  transition: all 0.4s;
-
-  &:hover {
-    background-color: rgba(255, 255, 255, 0.7);
-  }
-
-  svg {
-    margin-right: 0.5em;
-  }
-`;
 
 function App() {
   return (
     <>
-      <StyledTitle>
+      <h1 className="title">
         <SparklyText>
           <span>react-sparkly-text</span>
         </SparklyText>
-      </StyledTitle>
+      </h1>
 
-      <StyledGithubButton
+      <a
+        className="github-button"
         href="https://github.com/mburakerman/react-sparkly-text"
         target="_blank"
       >
@@ -58,7 +23,7 @@ function App() {
           <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
         </svg>{" "}
         Github
-      </StyledGithubButton>
+      </a>
     </>
   );
 }
