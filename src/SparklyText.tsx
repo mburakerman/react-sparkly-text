080667917b24fce0543b7640fa6a8bfc9c5db42a
Mehmet Burak Erman <mburakerman@gmail.com>
2023-12-22 15:34:36 +0300

    *

--- src/SparklyText.tsx
+++ src/SparklyText.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
+import "./styles/sparkly-text.css";
 
 const DEFAULT_SIZE = 30;
 
@@ -13,36 +13,6 @@ type Props = {
 type SparkleSVGProps = {
   timeout?: number; // in ms
 } & Pick<Props, "size">;
-
-const StyledContainer = styled.span`
-  position: relative;
-  width: max-content;
-  font-weight: bolder;
-`;
-
-const StyledSparkleSVG = styled.svg<{ color: string; size: Props["size"] }>`
-  position: absolute;
-  width: ${({ size }) => `${size}px`};
-  height: ${({ size }) => `${size}px`};
-  fill: ${({ color }) => color};
-  opacity: 0;
-  animation: sparkle 0.5s ease-in-out infinite alternate;
-  // generate random between -20 / 20
-  top: ${() => `calc(${Math.floor(Math.random() * 41) - 20}px)`};
-  // generate random between -20 / 120
-  left: ${() => `calc(${Math.floor(Math.random() * 121) - 20}%)`};
-
-  @keyframes sparkle {
-    0% {
-      opacity: 0;
-      transform: scale(0.25);
-    }
-    100% {
-      opacity: 1;
-      transform: rotateZ(30deg) scale(1);
-    }
-  }
-`;
 
 const SparkleSVG = ({ size, timeout = 1000 }: SparkleSVGProps) => {
   const [showSparkle, setShowSparkle] = useState(false);
@@ -62,20 +32,35 @@ const SparkleSVG = ({ size, timeout = 1000 }: SparkleSVGProps) => {
     return null;
   }
   return (
-    <StyledSparkleSVG viewBox="0 0 1200 1200" color={color} size={size}>
+    <svg
+      className="sparkly-text__sparkle-svg-x"
+      viewBox="0 0 1200 1200"
+      fill={color}
+      width={`${size}px`}
+      height={`${size}px`}
+      style={{
+        // generate random between -20 / 20
+        top: `calc(${Math.floor(Math.random() * 41) - 20}px)`,
+        // generate random between -20 / 120
+        left: `calc(${Math.floor(Math.random() * 121) - 20}%)`,
+      }}
+    >
       <path d="m611.04 866.16c17.418-61.09 50.25-116.68 95.352-161.42 45.098-44.742 100.94-77.133 162.17-94.062l38.641-10.68-38.641-10.68c-61.227-16.93-117.07-49.32-162.17-94.062-45.102-44.738-77.934-100.33-95.352-161.42l-11.039-38.641-11.039 38.641c-17.418 61.09-50.25 116.68-95.352 161.42-45.098 44.742-100.94 77.133-162.17 94.062l-38.641 10.68 38.641 10.68c61.227 16.93 117.07 49.32 162.17 94.062 45.102 44.738 77.934 100.33 95.352 161.42l11.039 38.641z" />
-    </StyledSparkleSVG>
+    </svg>
   );
 };
 
 export const SparklyText = ({ children, size = DEFAULT_SIZE }: Props) => {
   return (
-    <StyledContainer data-testid={`sparkly-text-${children.toString()}`}>
+    <span
+      className="sparkly-text__container-x "
+      data-testid={`sparkly-text-${children.toString()}`}
+    >
       {children}
       <SparkleSVG size={size} />
       <SparkleSVG size={size} timeout={2240} />
       <SparkleSVG size={size} timeout={2490} />
       <SparkleSVG size={size} timeout={2889} />
-    </StyledContainer>
+    </span>
   );
 };
